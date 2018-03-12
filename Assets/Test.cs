9ec9d85f1c1c227108b8c8492ba809f4a6262d28
city0409 <250924125@qq.com>
2018-03-12 10:47:33 +0800

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour 
+{
+    private Transform cube;
+	private void Awake() 
+	{
+        cube = GetComponent<Transform>();
+	}
+
+    private void Start()
+    {
+        StartCoroutine(Move(MoveFinish));
+    }
+
+    private void Update () 
+	{
+        if (cube.position.x<=10)
+        {
+            cube.Translate(new Vector3(0.1f, 0f, 0f));
+        }
+    }
+
+    private void MoveFinish()
+    {
+        print(cube.name);
+    }
+
+    private IEnumerator Move(Action MoveFinish)
+    {
+        while (cube.position.x<=10)
+        {
+            yield return null;
+        }
+
+        if (MoveFinish!=null)
+        {
+            MoveFinish();
+        }
+    }
+}
